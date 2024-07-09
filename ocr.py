599aefa8c4150589de1b7d6c7dbd086890c562a9
inada <inada.rai@fxc.jp>
2024-07-09 15:10:55 +0900

    画像貼り付け対応

--- ocr.py
+++ ocr.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PIL import Image, ImageGrab
+from PIL import Image
 import easyocr
 import numpy as np
 
@@ -15,25 +15,10 @@ def ocr(img):
 st.title("OCRアプリ")
 
 # Option to upload an image
-uploaded_file = st.file_uploader("画像ファイルを選択してください...", type=["jpg", "png", "jpeg"])
-
-# Option to paste an image from the clipboard
-if st.button("クリップボードから画像をペースト"):
-    try:
-        image = ImageGrab.grabclipboard()
-        if image is not None:
-            extracted_text = ocr(image)
-            st.subheader("OCR結果")
-            st.text(extracted_text)
-        else:
-            st.write("クリップボードに画像が見つかりません。")
-    except Exception as e:
-        st.write("クリップボードから画像をペースト中にエラーが発生しました: ", str(e))
+uploaded_file = st.file_uploader("画像ファイルを選択してください、または画像をここにドラッグしてください...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-
-    if st.button("アップロードされた画像でOCRを実行"):
-        extracted_text = ocr(image)
-        st.subheader("OCR結果")
-        st.text(extracted_text)
+    extracted_text = ocr(image)
+    st.subheader("OCR結果")
+    st.text(extracted_text)
